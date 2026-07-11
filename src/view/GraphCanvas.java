26ecf1ceeb130c7002ef83332a158fa445aa8950
Elizp36 <elizp3625@gmail.com>
2026-07-11 10:04:42 +0300

    Update GraphCanvas.java

--- src/view/GraphCanvas.java
+++ src/view/GraphCanvas.java
@@ -31,8 +31,8 @@ public class GraphCanvas extends JPanel{
         });
     }
 
-    //@Override
-    protected void  painComponent(Graphics g) {
+    @Override
+    protected void  paintComponent(Graphics g) {
         super.paintComponent(g);
 
         Graphics2D g2 = (Graphics2D) g;
