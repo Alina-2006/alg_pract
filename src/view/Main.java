7ba3edc3c9d51556b49e9cb634ccd8c58f5a647b
Elizp36 <elizp3625@gmail.com>
2026-07-11 09:54:05 +0300

    Create Main.java

---
+++ src/view/Main.java
@@ -0,0 +1,12 @@
+package view;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args){
+        SwingUtilities.invokeLater(() -> {
+            MainWindow window = new MainWindow();
+            window.setVisible(true);
+        });
+    }
+}
